e4dded597345c53fd863841d8b189f6865fdb3ba
Tien-Nam-Nguyen <nam18122002@outlook.com>
2024-03-26 18:23:18 +0700

    Add algo and rotate_left function

--- core.py
+++ core.py
@@ -30,7 +30,7 @@ class Tile:
         return self
     
 
-    def rotate(self, src=None, all_tiles=None):
+    def rotate_right(self, src=None, all_tiles=None):
         new_directions = {}
 
         for ele in self.directions:
@@ -40,27 +40,20 @@ class Tile:
 
         if src and all_tiles:
             return flow(src, all_tiles)
-
-
-
-
-def exist(x_target, y_target, stack):
-    if x_target < 0 or y_target < 0 or x_target >= GRID_SIZE or y_target >= GRID_SIZE:
-        return True
     
-    for tile in stack:
-        if tile.x == x_target and tile.y == y_target:
-            return True
+    
+    def rotate_left(self, src=None, all_tiles=None):
+        new_directions = {}
 
-    return False
+        for ele in self.directions:
+            new_directions[(ele + 3) % 4] = False
+        
+        self.directions = new_directions
+
+        if src and all_tiles:
+            return flow(src, all_tiles)
 
 
-def get_tile_index(x, y, stack):
-    for i, ele in enumerate(stack):
-        if ele.x == x and ele.y == y:
-            return i
-
-    return -1
 
 
 def flow(start, all_tiles):
@@ -68,6 +61,7 @@ def flow(start, all_tiles):
     num_wall = 0
     num_non_leak = 0
     num_water = 0
+    num_invalid_deadend = 0
 
     # Reset board state
     for tile in all_tiles:
@@ -104,8 +98,10 @@ def flow(start, all_tiles):
 
             
         if len(tile.directions) == 1 and side_idxs[0][1] != -1 and len(all_tiles[side_idxs[0][1]].directions) == 1:
-            num_wall += 1
+            num_invalid_deadend += 1
         
+        
+        # Compute number of closed head
         for way, index in side_idxs:
             if index != -1 and (way + 2) % 4 in all_tiles[index].directions:
                 tile.directions[way] = True
@@ -142,7 +138,7 @@ def flow(start, all_tiles):
 
 
     all_tiles[0].is_water = True
-    return num_head - num_non_leak, GRID_SIZE**2 - num_water, num_wall
+    return num_head - num_non_leak, GRID_SIZE**2 - num_water, num_wall, num_invalid_deadend
 
 
 
@@ -226,20 +222,139 @@ def generate_game_state(x_source, y_source, starts):   # [(x, y, direction)]
         rotate_time = random.randrange(4)
 
         for i in range(rotate_time):
-            num_leak, num_no_water, num_wall = tile.rotate(src, all_tiles) 
+            num_leak, num_no_water, num_wall, num_invalid_deadend = tile.rotate_right(src, all_tiles) 
 
 
-    print(f'-------------------{num_leak}--------------{num_no_water}---------------{num_wall}--------------')
+    print(f'-------------------{num_leak}--------------{num_no_water}---------------{num_wall}---------{num_invalid_deadend}-------')
     for ele in all_tiles:
         print(f'ele: {ele.x} {ele.y} {ele.directions} {ele.is_water}') 
         
     
 
 
-def algo(num_opened_head, num_no_water, num_wall_and_deadend):
-    fn = num_opened_head / 2 + num_no_water + num_wall_and_deadend
-    while True:
-        pass
+def algo(all_tiles, num_opened_head_, num_no_water_, num_wall_, num_invalid_deadend_):
+    # 
+    visited = []
+    checking_states = []        # [(state, hn, instructions, gn)]
+    hn = num_opened_head_ / 2 + num_no_water_ + num_wall_ * 4 + num_invalid_deadend_ * 2 
+    chosen_state = (all_tiles, hn, [], 0)
+
+    while not reach_goal(chosen_state):
+        
+
+
+        # Loop through all possible right states
+        for i, _ in enumerate(chosen_state[0]):
+            
+            right_lst = list_copy(chosen_state[0])
+            src = right_lst[0]
+            # Rotate right state
+            num_opened_head, num_no_water, num_wall, num_invalid_deadend = right_lst[i].rotate_right(src, right_lst)
+            hn = num_opened_head / 2 + num_no_water + num_wall * 4 + num_invalid_deadend * 2 
+            gn = chosen_state[3] + 1            # chosen_state[3]: previous gn
+            instructions = chosen_state[2].copy()
+            instructions.append((right_lst[i].x, right_lst[i].y, 'right'))
+
+            if not is_visited((right_lst, hn, instructions, gn), visited):
+                checking_states.append((right_lst, hn, instructions, gn))
+
+
+        
+            left_lst = list_copy(chosen_state[0])
+            src = left_lst[0]
+            # Rotate left state
+            num_opened_head, num_no_water, num_wall, num_invalid_deadend = left_lst[i].rotate_left(src, left_lst)
+            hn = num_opened_head / 2 + num_no_water + num_wall * 4 + num_invalid_deadend * 2 
+            gn = chosen_state[3] + 1
+            instructions = chosen_state[2].copy()
+            instructions.append((left_lst[i].x, left_lst[i].y, 'left'))
+
+            
+            if not is_visited((left_lst, hn, instructions, gn), visited):
+                checking_states.append((left_lst, hn, instructions, gn))
+
+            
+        curr_max = -1
+
+        for state in checking_states:
+            fn = state[1] + state[3]
+            if fn > curr_max:
+                curr_max = fn
+                chosen_state = state
+        
+        checking_states.remove(chosen_state)
+        visited.append(chosen_state)
+
+
+    return chosen_state[2]            # return a list of instructions to get to the goal state
+        
+
+
+def list_copy(lst):
+    ls = []
+    for ele in lst:
+        ls.append(create_tile_copy(ele))
+
+    return ls
+
+
+def create_tile_copy(tile):
+    tile_copy = Tile(tile.x, tile.y, tile.is_source, tile.is_water)
+    tile_copy.gate = tile.gate
+    tile_copy.directions = dict(tile.directions)
+    return tile_copy
+
+
+def is_visited(state, visited):
+    # state = (tiles, hn, instruction, gn)
+    # visite = [(tiles, hn, instruction, gn)]
+    
+    for visited_tiles, _, _, _ in visited:
+        for visited_tile, tile in zip(visited_tiles, state[0]):
+            if is_equal_tiles(visited_tile, tile):
+                return True
+            
+    return False
+    
+
+def is_equal_tiles(tile1, tile2):
+    if tile1.x == tile2.x and tile1.y == tile2.y and len(tile1.directions) == len(tile2.directions) and tile1.is_water == tile2.is_water:
+        for key, val in tile1.directions:
+            if key not in tile2.directions or tile2.directions[key] != val:
+                return False
+    else:
+        return False
+        
+    return True
+
+
+def reach_goal(chosen_state):
+    if chosen_state[1] == 0:
+        return True
+    
+    return False
+
+
+
+def exist(x_target, y_target, stack):
+    if x_target < 0 or y_target < 0 or x_target >= GRID_SIZE or y_target >= GRID_SIZE:
+        return True
+    
+    for tile in stack:
+        if tile.x == x_target and tile.y == y_target:
+            return True
+
+    return False
+
+
+def get_tile_index(x, y, stack):
+    for i, ele in enumerate(stack):
+        if ele.x == x and ele.y == y:
+            return i
+
+    return -1
+
+
 
 
 def main():
