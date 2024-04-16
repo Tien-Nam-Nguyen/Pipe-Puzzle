42f012630559479b18519fbcbe1a5af9ceb8ef85
smartkiiwii <nhan.nguyen1720dk@hcmut.edu.vn>
2024-04-16 09:54:18 +0700

    remove unused utils

--- bridges_puzzle/utils/__init__.py
+++ bridges_puzzle/utils/__init__.py
@@ -1,6 +1,5 @@
 from .add_bridge import add_bridge
 from .connect import connect
 from .create_game import create_game
-from .any_predicate import any_predicate
 from .is_intersecting import is_intersecting
 from .stringify import stringify
