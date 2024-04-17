e15dbd5a928936dbd3029ee7c93c60d74a9b74da
smartkiiwii <smartkiiwii>
2024-04-17 15:41:59 +0700

    use scaled button

--- bridges_puzzle/gui/Anchor.py
+++ bridges_puzzle/gui/Anchor.py
@@ -8,7 +8,8 @@ from pygame.rect import Rect
 from tweener import Tween, Easing, EasingMode, Ease
 
 from .GameObject import GameObject
-from .Button import Button, ButtonEvents
+from .Button import ButtonEvents
+from .ScaleButton import ScaleButton
 
 
 class Anchor(GameObject):
@@ -27,76 +28,21 @@ class Anchor(GameObject):
         self.radius = 0.0
         self.anim_delay = anim_delay
 
-        anim_appear = Tween(0, radius, 1000, Easing.ELASTIC, EasingMode.OUT)
-        self.anim_radius = anim_appear
+        self.anim_appear_radius = Tween(0, radius, 1000, Easing.ELASTIC, EasingMode.OUT)
+        self.rect = Rect(x - radius, y - radius, 2 * radius, 2 * radius)
 
-        self.anim_radius_start = None
-        self.anim_radius_target = None
-
-        self.button = Button(Rect(x - radius, y - radius, 2 * radius, 2 * radius))
-
-        self.basic_tween = Tween(0.0, 1.0, 200, Easing.QUAD, EasingMode.IN_OUT)
-
-        enter_target = radius * 1.2
-        rest_target = radius
-        down_target = radius * 1.3
-        up_target = radius
-
-        def handle_click():
-            pass
-
-        def handle_down():
-
-            self.basic_tween = Tween(0.0, 1.0, 500, Easing.ELASTIC, EasingMode.OUT)
-            self.basic_tween.start()
-            self.basic_tween.update()
-            self.anim_radius_start = self.radius
-            self.anim_radius_target = down_target
-
-        def handle_up():
-
-            self.basic_tween = Tween(0.0, 1.0, 500, Easing.ELASTIC, EasingMode.OUT)
-            self.basic_tween.start()
-            self.basic_tween.update()
-            self.anim_radius_start = self.radius
-            self.anim_radius_target = up_target
-
-        def handle_enter():
-
-            self.basic_tween = Tween(0.0, 1.0, 500, Easing.ELASTIC, EasingMode.OUT)
-            self.basic_tween.start()
-            self.basic_tween.update()
-            self.anim_radius_start = self.radius
-            self.anim_radius_target = enter_target
-
-        def handle_leave():
-
-            self.basic_tween = Tween(0.0, 1.0, 500, Easing.ELASTIC, EasingMode.OUT)
-            self.basic_tween.start()
-            self.basic_tween.update()
-            self.anim_radius_start = self.radius
-            self.anim_radius_target = rest_target
-
-        self.button.on(ButtonEvents.CLICK, handle_click)
-        self.button.on(ButtonEvents.DOWN, handle_down)
-        self.button.on(ButtonEvents.UP, handle_up)
-        self.button.on(ButtonEvents.ENTER, handle_enter)
-        self.button.on(ButtonEvents.LEAVE, handle_leave)
+        self.button = ScaleButton(self.rect, 1.2, 1.3)
 
     def start(self):
         self.start_time = get_ticks()
+        self.button.on(ButtonEvents.CLICK, lambda: print(f"clicked {self.value}"))
 
     def render(self, delta_time: float, time: float):
         if self.value is None:
             rect(
                 self.screen,
                 Color(230, 230, 230),
-                Rect(
-                    self.x - self.radius,
-                    self.y - self.radius,
-                    2 * self.radius,
-                    2 * self.radius,
-                ),
+                self.rect,
                 width=2,
                 border_radius=20,
             )
@@ -106,12 +52,7 @@ class Anchor(GameObject):
         rect(
             self.screen,
             Color(230, 230, 230),
-            Rect(
-                self.x - self.radius,
-                self.y - self.radius,
-                2 * self.radius,
-                2 * self.radius,
-            ),
+            self.rect,
             border_radius=20,
         )
 
@@ -124,7 +65,11 @@ class Anchor(GameObject):
         text.set_alpha(
             min(
                 255,
-                int(round(self.anim_radius.value / max(self.radius, 0.01) * 255, 0)),
+                int(
+                    round(
+                        self.anim_appear_radius.value / max(self.radius, 0.01) * 255, 0
+                    )
+                ),
             )
         )
         self.screen.blit(
@@ -133,25 +78,27 @@ class Anchor(GameObject):
 
     def update(self, delta_time: float, time: float):
         if time - self.start_time > self.anim_delay:
-            self.anim_radius.start()
+            self.anim_appear_radius.start()
             # set infinity so that it doesn't start again
             self.start_time = float("inf")
 
-        if self.anim_radius.animating:
-            self.anim_radius.update()
-            self.radius = self.anim_radius.value
-
-        if self.basic_tween.animating:
-            self.basic_tween.update()
-            self.radius = self.anim_radius_start + self.basic_tween.value * (
-                self.anim_radius_target - self.anim_radius_start
+        if self.anim_appear_radius.animating:
+            self.anim_appear_radius.update()
+            self.radius = self.anim_appear_radius.value
+            self.rect.update(
+                self.x - self.radius,
+                self.y - self.radius,
+                2 * self.radius,
+                2 * self.radius,
             )
 
-        self.button.rect = Rect(
-            self.x - self.anim_radius.value,
-            self.y - self.anim_radius.value,
-            2 * self.anim_radius.value,
-            2 * self.anim_radius.value,
-        )
+            return
 
         self.button.update(delta_time, time)
+        rect = self.button.scaled_rect
+        self.rect.update(
+            rect.x,
+            rect.y,
+            rect.width,
+            rect.height,
+        )
